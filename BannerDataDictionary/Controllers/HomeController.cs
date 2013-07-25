0f0f0e41df6ce067bce938672250191a3e55d555
Ken Taylor <kentaylor@ucdavis.edu>
2013-07-25 14:46:30 -0700

    Moved things back to Home

--- BannerDataDictionary/Controllers/HomeController.cs
+++ BannerDataDictionary/Controllers/HomeController.cs
@@ -2,35 +2,51 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Dynamic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using BannerDataDictionary.Models;
 using Dapper;
 
 namespace BannerDataDictionary.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApplicationController
     {
-
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-            return View();
+            ViewBag.Message = "Banner Tables";
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MainDb"].ConnectionString))
+            {
+                conn.Open();
+                IEnumerable<Table> resultList = conn.Query<Table>(@"EXEC usp_GetBannerTableNamesAndComments");
+                return View(resultList);
+            }
         }
 
-        public ActionResult About()
+        public ActionResult Details(string id)
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Table Details";
+            var viewModel = new TableDetailsModel { TableName = id };
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MainDb"].ConnectionString))
+            {
+                conn.Open();
+                IEnumerable<Table> tables = conn.Query<Table>(@"EXEC usp_GetBannerTableNamesAndComments @TableNames = '" + id + "'");
+                IEnumerable<Columns> columnsList = conn.Query<Columns>(@"EXEC usp_GetBannerColumnNamesCommentsAndDataTypes @TableNames = '" + id + "'");
 
-            return View();
+                var firstOrDefault = tables.FirstOrDefault();
+                if (firstOrDefault != null)
+                {
+                    viewModel.Owner = firstOrDefault.Owner;
+                    viewModel.TableComments = firstOrDefault.Comments ?? String.Empty;
+                    viewModel.NumRows = firstOrDefault.NumRows;
+                }
+                viewModel.Columns = columnsList.ToList();
+
+                return View(viewModel);
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult ColumnLocations(string id)
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
